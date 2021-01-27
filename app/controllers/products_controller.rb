d6e8360aa7e256942b57c72ebb05635cf1a346f7
tatsuya06068 <fhkt863@gmail.com>
2021-01-27 21:13:59 +0900

    consoleを導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
   def index
+    console
     @q = Product.ransack(params[:q])
     @materials = Material.all
-    @products = @q.result.includes(:product_detail, :materials_useds, :materials)
+    @products = @q.result.includes(:product_detail, materials: :materials_useds)
     # Product.includes([:product_details, materials_useds: :materias])
   end
 
