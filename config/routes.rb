ad2613af730e094a9491dfaea812c305eae47274
tatsuya06068 <fhkt863@gmail.com>
2021-02-11 20:54:57 +0900

    adminサインイン完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    resources :admin
+  end
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
