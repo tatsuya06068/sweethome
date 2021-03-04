367e40ca98ea2f8dcbd7ce456f01cfb29a4d8cee
tatsuya06068 <fhkt863@gmail.com>
2021-03-05 05:32:03 +0900

    商品一覧実装

--- app/helpers/productlist_helper.rb
+++ app/helpers/productlist_helper.rb
@@ -1,2 +1,15 @@
 module ProductlistHelper
+
+  def join_materials(materials)
+    
+    array = Array.new
+    
+    materials.each do |material|
+      array.push(material.name)
+    end
+    material_join = array.join(',')
+
+   
+
+  end
 end
